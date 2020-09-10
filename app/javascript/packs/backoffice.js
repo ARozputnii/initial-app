f7df34f07e54d6c1e416c96398a7c3ceb4adc242
ARozputnii <arozputnii@gmail.comt@gitlab.com:everlabs/everlabs-website.gitset :rvm_ruby_version, 2.6.5set :user, deployer>
2020-09-10 20:07:44 +0300

    style/ removal of excess

--- app/javascript/packs/backoffice.js
+++ app/javascript/packs/backoffice.js
@@ -2,6 +2,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 import "bootstrap";
 
@@ -11,5 +12,4 @@ import "../stylesheets/backoffice/sessions.scss";
 import "../stylesheets/backoffice/sidebar.scss";
 
 // js
-import "./bootstrap_custom";
 import "@fortawesome/fontawesome-free/js/all";
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   get 'backoffice', to: 'backoffice/sessions#new'
   post 'backoffice', to: 'backoffice/sessions#create'
 
-  resources :posts
 
   scope '/backoffice' do
     get 'backoffices', to: 'backoffice/backoffices#home'
-    resources :posts, controller: 'backoffice/posts', as: 'backoffice_posts'
-    resources :articles, controller: 'backoffice/articles', as: 'backoffice_articles'
+    resources :posts, controller: 'backoffice/posts', as: 'backoffice_posts' do
+      resources :comments, controller: 'backoffice/comments', as: 'backoffice_comments'
+    end
     resources :admins, controller: 'backoffice/admins', as: 'backoffice_admins'
   end
 end
