03e2c2f059951a746c737f2ea694cf7c1ce1ee07
ARozputnii <arozputnii@gmail.comt@gitlab.com:everlabs/everlabs-website.gitset :rvm_ruby_version, 2.6.5set :user, deployer>
2020-11-02 12:48:43 +0200

    restart puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,13 +34,11 @@ namespace :set do
   end
 end
 
-bundle_wrapper_path = "/home/deployer/.rvm/gems/ruby-2.7.1/wrappers/bundle"
-
 namespace :restart_puma do
   desc "Restart puma"
   task :restart do
     on roles(:app) do
-      execute "cd #{release_path} && #{bundle_wrapper_path} exec pumactl -P #{shared_path}/tmp/pids/puma.pid restart"
+      execute "cd && sudo service puma restart"
     end
   end
 end
